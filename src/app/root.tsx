0a85793055146470e88e4b3ef4b4a34e6111d6e8
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-09 13:40:38 -0400

    add app skeleton

---
+++ src/app/root.tsx
@@ -0,0 +1,24 @@
+
+import { createRoot } from 'react-dom/client';
+
+import { App } from '@/app/components/App';
+
+import '@/app/root.css';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+async function enableMocking() {
+  // if (!import.meta.env.DEV) return;
+  // const workerStartOptions: StartOptions = {
+  //   onUnhandledRequest: 'error',
+  //   serviceWorker: {
+  //     url: './mockServiceWorker.js',
+  //   },
+  // };
+  // const { worker } = await import('@/app/mocks/worker');
+  // return worker.start(workerStartOptions);
+}
+
+enableMocking().then(() => {
+  root.render(<App />);
+});

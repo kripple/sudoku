9707cca6c7c362971a140b4d931dbdcc70a5b387
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-10 13:52:01 -0400

    reorder tokens

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,13 +1,13 @@
 export const tokens = {
-  '1': 'fire',
-  '2': 'ghost',
+  '1': 'fairy',
+  '2': 'fire',
   '3': 'electric',
   '4': 'grass',
   '5': 'ice',
   '6': 'water',
   '7': 'flying',
   '8': 'poison',
-  '9': 'fairy',
+  '9': 'ghost',
 } as const;
 
 export type Token = keyof typeof tokens;

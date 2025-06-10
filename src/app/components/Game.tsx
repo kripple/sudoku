c458bcd924aedfe7f8321b8d438ea35b3ce508cd
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-09 20:24:38 -0400

    add sudoku-gen

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,41 +1,41 @@
 import { getSudoku } from 'sudoku-gen';
+import { useState } from 'react';
 
 import '@/app/components/Game.css';
 
 const gameSize = 9 as const;
-const gameRows = new Array(gameSize)
-  .fill(0)
-  .map(() => new Array(gameSize).fill('-'));
 
 export function Game() {
+  const [sudoku, setSudoku] = useState<ReturnType<typeof getSudoku>>(
+    getSudoku('easy'),
+  );
+  const cells = sudoku.puzzle.split('');
+  console.log(sudoku.puzzle)
+
   return (
     <div className="game">
-      {gameRows.map((columns, x) =>
-        columns.map((_column, y) => {
-          const rowId = x + 1;
-          const columnId = y + 1;
+      {cells.map((cell, i) => {
+        const rowId = (i % gameSize) + 1;
+        const columnId = Math.floor(i / gameSize) + 1;
 
-          const isBorderCell = (id: number) => id % Math.sqrt(gameSize) === 0;
-          const rowBorder = isBorderCell(rowId) ? 'border-bottom' : false;
-          const columnBorder = isBorderCell(columnId) ? 'border-right' : false;
-
-          const classNames = [
-            'game-cell',
-            `row-${rowId}`,
-            `col-${columnId}`,
-            rowBorder,
-            columnBorder,
-          ]
-            .filter(Boolean)
-            .join(' ');
-
-          return (
-            <div key={`row-${rowId}-col-${columnId}`} className={classNames}>
-              {/* ({rowId}, {columnId}) */}
-            </div>
-          );
-        }),
-      )}
+        const isBorderCell = (id: number) => id % Math.sqrt(gameSize) === 0;
+        const rowBorder = isBorderCell(rowId) ? 'border-right' : false;
+        const columnBorder = isBorderCell(columnId) ? 'border-bottom' : false;
+        const classNames = [
+          'game-cell',
+          `row-${rowId}`,
+          `col-${columnId}`,
+          rowBorder,
+          columnBorder,
+        ]
+          .filter(Boolean)
+          .join(' ');
+        return (
+          <div key={`row-${rowId}-col-${columnId}`} className={classNames}>
+            {cell}
+          </div>
+        );
+      })}
     </div>
   );
 }

65c0006941192ea0eafea318196472518bb86316
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-10 20:25:01 -0400

    add keyboard listeners

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,39 +1,92 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getSudoku } from 'sudoku-gen';
 
 import { Cell } from '@/app/components/Cell';
-import { emptyCell } from '@/constants/config';
+import { useStateRef } from '@/app/hooks/useStateRef';
+import { emptyCell, gameSize } from '@/constants/config';
+import { replaceAt } from '@/utils/string-replace';
 
 import '@/app/components/Game.css';
 
 export function Game() {
-  const [sudoku] = useState<ReturnType<typeof getSudoku>>(getSudoku('easy'));
+  const [sudoku, _setSudoku, sudokuRef] = useStateRef<
+    ReturnType<typeof getSudoku>
+  >(getSudoku('easy'));
   const cells = sudoku.puzzle.split('');
   const solution = sudoku.solution.split('');
 
   const [input, setInput] = useState<string>(sudoku.puzzle);
   const inputs = input.split('');
 
-  const [selected, setSelected] = useState<number>(
+  const [selected, setSelected, selectedRef] = useStateRef<number>(
     cells.findIndex((value) => value === emptyCell),
   );
 
+  const handleArrowUp = () => {
+    setSelected((current) => {
+      const colId = Math.floor(current / gameSize) + 1;
+      return colId === 1 ? current : current - gameSize;
+    });
+  };
+
+  const handleArrowDown = () => {
+    setSelected((current) => {
+      const colId = Math.floor(current / gameSize) + 1;
+      return colId === gameSize ? current : current + gameSize;
+    });
+  };
+
+  const handleArrowRight = () => {
+    setSelected((current) => {
+      const rowId = (current % gameSize) + 1;
+      return rowId === gameSize ? current : current + 1;
+    });
+  };
+
+  const handleArrowLeft = () => {
+    setSelected((current) => {
+      const rowId = (current % gameSize) + 1;
+      return rowId === 1 ? current : current - 1;
+    });
+  };
+
+  const handleBackspace = () => {
+    const readonlyCells = sudokuRef.current.puzzle.split('');
+    const isEditable = readonlyCells[selectedRef.current] === emptyCell;
+    setInput((draft) =>
+      isEditable ? replaceAt(draft, selectedRef.current, emptyCell) : draft,
+    );
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const keys = {
+        ArrowUp: handleArrowUp,
+        ArrowDown: handleArrowDown,
+        ArrowRight: handleArrowRight,
+        ArrowLeft: handleArrowLeft,
+        Backspace: handleBackspace,
+      };
+      if (event.key in keys) keys[event.key as keyof typeof keys]();
+    };
+    document.body.addEventListener('keydown', handleKeyDown);
+    return () => document.body.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
   return (
     <div className="game">
-      {inputs.map((input, i) => {
-        return (
-          <Cell
-            currentValue={input}
-            index={i}
-            initialValue={cells[i]}
-            key={i}
-            selected={i === selected}
-            setInput={setInput}
-            setSelected={setSelected}
-            solution={solution[i]}
-          />
-        );
-      })}
+      {inputs.map((input, i) => (
+        <Cell
+          currentValue={input}
+          index={i}
+          initialValue={cells[i]}
+          key={i}
+          selected={i === selected}
+          setInput={setInput}
+          setSelected={setSelected}
+          solution={solution[i]}
+        />
+      ))}
     </div>
   );
 }

d85c918e4a4fed22861b8b20ecf6cd27e14e0da7
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-13 00:38:51 -0400

    add simplistic dark mode

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { CursorSelect } from '@/app/components/CursorSelect';
 import { Game } from '@/app/components/Game';
 import { CursorProvider } from '@/app/providers/CursorProvider';
@@ -5,6 +7,13 @@ import { CursorProvider } from '@/app/providers/CursorProvider';
 import '@/app/components/App.css';
 
 export function App() {
+  useEffect(() => {
+    const now = new Date();
+    const hours = now.getHours();
+    const dark = hours < 8 || hours > 20;
+    document.documentElement.style.filter = dark ? 'brightness(0.7)' : 'none';
+  }, []);
+
   return (
     <CursorProvider>
       <main className="main">
